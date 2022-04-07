815e953d22c64b13a86519da19853b79f84d469a
Zach Carter <thezachcarter@gmail.com>
2022-04-07 09:16:18 -0500

    add fav button to dom

--- src/components/SearchListItem/SearchListItem.jsx
+++ src/components/SearchListItem/SearchListItem.jsx
@@ -4,10 +4,15 @@ import {useDispatch, useSelector} from 'react-redux';
 function SearchListItem({image}) {
     console.log('image:', image);
     
+    const handleFavorite = () => {
+        console.log('clicked fav');
+    }
+
     return(
         <>
             <div>
                 <img src={image.images.original.url}/>
+                <button onClick={handleFavorite}>💖</button>
             </div>
         </>
         )
