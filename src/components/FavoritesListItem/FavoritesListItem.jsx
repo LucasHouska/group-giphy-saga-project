0b55620563b56e1036dd8c359e23d508567c4d7b
Zach Carter <thezachcarter@gmail.com>
2022-04-07 11:26:10 -0500

    add fully functional delete button

--- src/components/FavoritesListItem/FavoritesListItem.jsx
+++ src/components/FavoritesListItem/FavoritesListItem.jsx
@@ -6,10 +6,17 @@ function FavoritesListItem({favoriteImage}) {
     console.log('favorite image:', favoriteImage);
     console.log('favorite url:', favoriteImage.url);
 
+    const dispatch = useDispatch();
+
+    const removeFavorite = () => {
+        dispatch({type: 'REMOVE_FAVORITE', payload: favoriteImage.id});
+    }
+
     return(
         <>
         <div>
             <img src={favoriteImage.url}/>    
+            <button onClick={removeFavorite}>💔</button>
         </div>
         </>
         )
